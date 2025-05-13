09604027048f90d62eda14307e8e2a0e14c92079
Shah <rajan-kumar.shah@capgemini.com>
2025-05-13 18:32:45 +0530

    Chapter 7: Adding custom response for Error with proper message

--- section7/springsecsection7/src/main/java/com/eazybytes/springsecsection7/exceptions/CustomBasicAuthenticationEntryPoint.java
+++ section7/springsecsection7/src/main/java/com/eazybytes/springsecsection7/exceptions/CustomBasicAuthenticationEntryPoint.java
@@ -8,11 +8,23 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 public class CustomBasicAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        LocalDateTime currentTimeStamp = LocalDateTime.now();
+        String message = (authException!=null && authException.getMessage()!=null) ? authException.getMessage() : "Unauthorized";
+        String path = request.getRequestURI();
+
+        String jsonResponse =
+                String.format("{\"timestamp\": \"%s\", \"status\": %d, \"error\": \"%s\", \"message\": \"%s\", \"path\": \"%s\"}",
+                        currentTimeStamp, HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED.getReasonPhrase(),
+                        message, path);
+
         response.setHeader("ezybank-error-authenticate", "Authentication Failed");
-        response.sendError(HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED.getReasonPhrase());
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.setContentType("application/json;charset=UTF-8");
+        response.getWriter().write(jsonResponse);
     }
 }
